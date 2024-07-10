51dea5737c3fdcd83d36d194417f46ccfcd27e9b
John Kerl <kerl.john.r@gmail.com>
2024-07-10 15:41:30 +0000

    core schema dump -> stringstream

--- libtiledbsoma/test/unit_soma_group.cc
+++ libtiledbsoma/test/unit_soma_group.cc
@@ -97,7 +97,9 @@ std::tuple<std::string, uint64_t> create_array(
     // Open array for writing
     Array array(ctx, uri, TILEDB_WRITE, TemporalPolicy(TimeTravel, timestamp));
     if (LOG_DEBUG_ENABLED()) {
-        array.schema().dump();
+        std::stringstream ss;
+        ss << array.schema();
+        std::cout << ss.str();
     }
 
     // Generate fragments in random order
